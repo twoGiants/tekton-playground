89f5c6c7d8b2f2daa2e76e79c9474f26884de755
Stanislav Jakuschevskij <stas@two-giants.com>
2025-05-19 12:19:48 +0200

    test(operator): fail deployment creation test

--- samples/memcached-operator/internal/controller/memcached_controller_test.go
+++ samples/memcached-operator/internal/controller/memcached_controller_test.go
@@ -18,11 +18,17 @@ package controller
 
 import (
 	"context"
+	"errors"
+
+	appsv1 "k8s.io/api/apps/v1"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -33,9 +39,7 @@ import (
 var _ = Describe("Memcached Controller", func() {
 	Context("When reconciling a resource", func() {
 		const resourceName = "test-resource"
-
 		ctx := context.Background()
-
 		typeNamespacedName := types.NamespacedName{
 			Name:      resourceName,
 			Namespace: "default", // TODO(user):Modify as needed
@@ -45,7 +49,7 @@ var _ = Describe("Memcached Controller", func() {
 		BeforeEach(func() {
 			By("creating the custom resource for the Kind Memcached")
 			err := k8sClient.Get(ctx, typeNamespacedName, memcached)
-			if err != nil && errors.IsNotFound(err) {
+			if err != nil && apierrors.IsNotFound(err) {
 				resource := &cachev1alpha1.Memcached{
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      resourceName,
@@ -124,6 +128,70 @@ var _ = Describe("Memcached Controller", func() {
 		})
 	})
 
+	Context("When reconciling a resource and setting controller reference fails", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		memcached := &cachev1alpha1.Memcached{}
+
+		errMsg := "Failed setting controller reference"
+		setControllerReferenceFake := func(_, _ metav1.Object, _ *runtime.Scheme, _ ...controllerutil.OwnerReferenceOption) error {
+			return errors.New(errMsg)
+		}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind Memcached")
+			err := k8sClient.Get(ctx, typeNamespacedName, memcached)
+			if err != nil && apierrors.IsNotFound(err) {
+				resource := &cachev1alpha1.Memcached{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					Spec: cachev1alpha1.MemcachedSpec{
+						Size: 1,
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			resource := &cachev1alpha1.Memcached{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance Memcached")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+
+		It("should set resource status to 'False' when setting controller reference fails", func() {
+			controllerReconciler := &MemcachedReconciler{
+				Client:                 k8sClient,
+				Scheme:                 k8sClient.Scheme(),
+				SetControllerReference: setControllerReferenceFake,
+			}
+
+			By("Reconcile the resource first time")
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal(errMsg))
+
+			By("Status 'False' after first reconciliation loop")
+			updated := &cachev1alpha1.Memcached{}
+			Expect(k8sClient.Get(ctx, typeNamespacedName, updated)).To(Succeed())
+			Expect(updated.Status.Conditions[0].Status).To(Equal(metav1.ConditionFalse))
+			Expect(updated.Status.Conditions[0].Reason).To(Equal("Reconciling"))
+		})
+	})
+
 	Context("When reconciling a non existing resource", func() {
 		It("should not find the resource and stop the reconciliation loop", func() {
 			ctx := context.Background()
@@ -145,7 +213,7 @@ var _ = Describe("Memcached Controller", func() {
 
 			err = k8sClient.Get(ctx, typeNamespacedName, &cachev1alpha1.Memcached{})
 			Expect(err).To(HaveOccurred())
-			Expect(errors.IsNotFound(err)).To(BeTrue())
+			Expect(apierrors.IsNotFound(err)).To(BeTrue())
 		})
 	})
 })
